f803aa2b19aa66108144ae7af32b03ddb13286b5
Heber Jafet Jafet <114103375+Heberjfet@users.noreply.github.com>
2022-09-28 14:06:24 +0000

    ESTUDIAR

--- script-126.py
+++ script-126.py
@@ -1,7 +1,6 @@
 #escribe un programa que imprima en pantalla la frase S, N y terminar hasta que la respuesta se a S o N
 #calcular el factorial de un numero N la mul d num por un num inferior
-x = input('desea continuar: N= no, S = si ')
+x = input('desea continuar: ')
 while x != 'S':
-    print('desea continuar')
-    x != 'S'
     print('gracias por contribuir')
+    x = input("desea continuar: ")
