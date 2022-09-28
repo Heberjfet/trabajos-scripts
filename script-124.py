5643bb70ade11184c1de04df7833ac721dd9530f
Heberjfet <114103375+Heberjfet@users.noreply.github.com>
2022-09-28 07:48:42 -0500

    actividad

--- script-124.py
+++ script-124.py
@@ -1,1 +1,13 @@
-#escribe un programa para calcilar cada uno de los valores de y a la aecuacion y es igual a 3x+5 en donde x toma valores de x 0 a 10 e incremento de 0.5
+#programa : script-124
+#programador : Heber Jafet Alvaro Ramirez
+#descripcion:un programa para calcilar cada uno de los valores de y a la aecuacion y es igual a 3x+5 en donde x toma valores de x 0 a 10 e incremento de 0.5
+#fecha: 28/09/2002
+
+x = 0
+while (x<= 10):
+    y = 3* + 5
+    x += 0.5
+    print(x,y)
+print('termino')
+
+
