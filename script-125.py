84ff5a04bb3849567ec424b21be8ea4acfa90178
Heber Jafet Jafet <114103375+Heberjfet@users.noreply.github.com>
2022-09-28 13:57:58 +0000

    estudiar while

---
+++ script-125.py
@@ -0,0 +1,12 @@
+##programa : script-125
+#programador : Heber Jafet Alvaro Ramirez
+#descripcion:
+#fecha: 28/09/2002
+
+print('ciclo whlie')
+x = int(input('ciclo'))
+while x != 1:
+    if x % 1:
+        x * 3 + 1
+    else:
+        x / 2
---
+++ script-126.py
@@ -0,0 +1,7 @@
+#escribe un programa que imprima en pantalla la frase S, N y terminar hasta que la respuesta se a S o N
+#calcular el factorial de un numero N la mul d num por un num inferior
+x = input('desea continuar: N= no, S = si ')
+while x != 'S':
+    print('desea continuar')
+    x != 'S'
+    print('gracias por contribuir')
