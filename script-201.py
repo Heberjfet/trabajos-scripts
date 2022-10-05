8a4d4b8769c75843408e68c64a5d39a3cb1cbd55
Heber Jafet Jafet <114103375+Heberjfet@users.noreply.github.com>
2022-10-05 12:46:58 +0000

    script200

---
+++ script-201.py
@@ -0,0 +1,11 @@
+#programa: script-128
+#programador: Heber Jafet Alvaro Ramirez
+#decripcion:
+#fecha: 5/10/2022
+n = int(input('ingresa el valor de n:'))
+s = 0
+i = 1
+while (i <= n):
+    s += i
+    i += 1
+print('resultado:', s)
