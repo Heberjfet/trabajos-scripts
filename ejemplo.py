7f24c965c3c1c9a98a0cc5f507472a4437e3acd4
heberjfet <heber.alvaro49@unach.mx>
2022-10-06 12:27:09 -0500

    ejemplo

---
+++ ejemplo.py
@@ -0,0 +1,12 @@
+from cgitb import lookup
+from numbers import Real
+
+
+angulo = int(input('ingrese dato'))
+senAng = int(input('ingrese dato'))
+cosAng : Real
+lookup= angulo
+calculo = senAng(angulo)
+calculo2 = cosAng(angulo)
+print(senAng,cosAng)
+end = 0,""
