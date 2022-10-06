54d67d1baf5411a3382a949282d2fe9f27b59be3
heberjfet <heber.alvaro49@unach.mx>
2022-10-06 00:33:56 -0500

    sceiprt200

--- script-204.py
+++ script-204.py
@@ -2,3 +2,21 @@
 #programador: Heber Jafet Alvaro Ramirez
 #decripcion:
 #fecha: 05/10/2022
+
+n1 = int (input('dijite un valor: '))
+n2 = int (input('dijite un valor: '))
+r = []
+i = 1
+for r in i <= n1 / 2:
+    n1 % i == 0
+    s1 = s1 + 1
+    i = i + 1
+    i = 1
+    i <= n2 / 2
+    n2 % i == 0
+    s2 = s2 + i
+    i = i + 1
+    n1 == s2 and n2==s1
+    r = ('no son amigos')
+    r = ('son amigos')
+print (r)
