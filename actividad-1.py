3200b82e05a3a204458bd87df2ab3f10c7587f6d
heberjfet <heber.alvaro49@unach.mx>
2022-10-06 12:44:48 -0500

    activisdad1

--- actividad-1.py
+++ actividad-1.py
@@ -1,8 +1,24 @@
 def mensaje():
     print('ingresa el sig valor:')
     return
-print('comenzamos aqui')
-mensaje()
-mensaje()
-mensaje()
-print('el final es aqui')
+    print('comenzamos aqui')
+# mensaje()
+# mensaje()
+# mensaje()
+    print('el final es aqui')
+
+def saludar():
+    print('buenos dias')
+    print('hola')
+# saludar()
+
+def suma():
+    print('dame un numero')
+    num1 =int(input('valor:'))
+    print('dame otro numero')
+    num2 = int(input('valor: '))
+    resultado = num1 + num2
+    print('el resultado de la suma es:', resultado)
+print('operacion de suma')
+suma()
+
