934a3136080d708a897ea511f7437cdc09846319
heberjfet <heber.alvaro49@unach.mx>
2022-10-11 00:13:28 -0500

    sd

--- script-210.py
+++ script-210.py
@@ -3,19 +3,31 @@ def suma():
     nunA = int(input('ingrese dato: '))
     numB = int(input('ingrese dato: '))
     sum = nunA + numB
-    print (sum)
+    print ('el resultado es: ',sum)
 
 
 def resta():
     numA = int(input('ingrese dato'))
     numB = int(input('ingrese dato: '))
     res = numA - numB
-    print(res)
+    print('el resultado es: ',res)
 
 
 def multiplicacion():
     numA = int(('ingrese dato: '))
     numB = int(('ingrese dato: '))
     mult = numA * numB
-    print(mult)
+    print('el resultado es: ',mult)
 
+print("""
+s = suma
+r = resta
+m = multiplicacion
+d = divicion
+""")
+selec = input('seleccione la opereacion a reaalizar: ')
+
+if selec == 's':
+    suma()
+else selec == 'r':
+    resta()
