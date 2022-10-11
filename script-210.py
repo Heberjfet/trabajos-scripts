5ff3799b3de3a5e92273111a65fc387d9a787d68
heberjfet <heber.alvaro49@unach.mx>
2022-10-11 00:08:47 -0500

    suma

--- script-210.py
+++ script-210.py
@@ -1,33 +1,21 @@
 #prueba
 def suma():
-    nunA = input('ingrese dato: ')
-    numB = input('ingrese dato: ')
+    nunA = int(input('ingrese dato: '))
+    numB = int(input('ingrese dato: '))
     sum = nunA + numB
     print (sum)
-    suma()
+
 
 def resta():
-    numA = input('ingrese dato')
-    numB = input('ingrese dato: ')
+    numA = int(input('ingrese dato'))
+    numB = int(input('ingrese dato: '))
     res = numA - numB
     print(res)
-    resta()
+
 
 def multiplicacion():
-    numA = ('ingrese dato: ')
-    numB = ('ingrese dato: ')
+    numA = int(('ingrese dato: '))
+    numB = int(('ingrese dato: '))
     mult = numA * numB
     print(mult)
-    multiplicacion()
 
-
-
-print("""
-s = suma
-r = resta
-m = multiplicacion
-d = divicion
-""")
-selec = input('seleccione la opereacion a reaalizar: ')
-if selec == 's':
-    suma()
