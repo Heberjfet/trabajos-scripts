15a15b2e7b0d8236a61f2561f0cffb5baeeaa45b
Heberjfet <114103375+Heberjfet@users.noreply.github.com>
2022-10-13 12:34:33 -0500

    script

--- script-211.py
+++ script-211.py
@@ -2,3 +2,12 @@
 #script: script 211
 #detalle:
 #fecha: 13/10/2022
+def suma(a,b):
+    print('la suma es:',a+b)
+
+print('dame un valor')
+val1 = int(input())
+print('dame un valor:')
+val2 = int(input())
+
+suma(val1,val2)
