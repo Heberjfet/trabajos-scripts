b3306954098041f89800d583f82ad1a9e3f05e83
Heber Jafet Jafet <114103375+Heberjfet@users.noreply.github.com>
2022-10-17 15:32:13 +0000

    rs

--- script-213.py
+++ script-213.py
@@ -1,5 +1,12 @@
 def saludar(*nombres):
   for n in nombres:
     print(n)
+#saludar('marta','mario', 'pedro')
 
-saludar('marta','mario', 'pedro')
+def sumadelista(lst):
+  sum=0
+  for elem in lst:
+    sum += elem
+    return sum
+
+print(sumadelista([5,4,3]))
