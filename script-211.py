523dcdca3a8400f08ee2ab738f5778e8961ec64c
heberjfet <heber.alvaro49@unach.mx>
2022-10-17 06:15:44 -0500

    tarea

--- script-211.py
+++ script-211.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#programa: script-211
-#programador: Heber Jafet Alvaro Ramirez
-#decripcion: años viciestos
-#fecha: 16/10/2022
-
-print('calculador de año biciestos')
-
-info =int(input('ingrese el año:'))
-ex=0
-while info == 0:
-    input('ingrese el año')
-
-def años():
-    if años % 4 == 0:
-        print('es aun año biciesto')
-    else:
-        print('no es un año biciesto')
-=======
 #programador:heber jafet alvaro ramirez
 #script: script 211
 #detalle:
@@ -30,4 +11,4 @@ print('dame un valor:')
 val2 = int(input())
 
 suma(val1,val2)
->>>>>>> ad789807f37bde59a4ea04728656f208e944caf0
+
--- script-212.py
+++ script-212.py
@@ -1,1 +1,18 @@
 #escribir y probar una funcion que reciba un año como argumento y devuelve true si el año es un año bisisesto, o flase si no lo es.enviar varios años hasta que se teclee un valor 0
+#programa: script-211
+#programador: Heber Jafet Alvaro Ramirez
+#decripcion: años viciestos
+#fecha: 16/10/2022
+
+print('calculador de año biciestos')
+
+info =int(input('ingrese el año:'))
+ex=0
+while info == 0:
+    input('ingrese el año')
+
+def años():
+    if años % 4 == 0:
+        print('es aun año biciesto')
+    else:
+        print('no es un año biciesto')
