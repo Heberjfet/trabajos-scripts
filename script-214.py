3eb9eb9de11c46f28766feb3c76dafc2fd66644e
heberjfet <heber.alvaro49@unach.mx>
2022-10-18 22:42:43 -0500

    revisar mañana

--- script-214.py
+++ script-214.py
@@ -2,3 +2,11 @@
 #programador: Heber Jafet Alvaro Ramirez
 #decripcion: numeros primos
 #fecha: 16/10/2022
+
+def numeros_primos(numero):
+    for i in range (1,int(numero/2)):
+        if (numero % i) == 0:
+            print(i, 'es primo')
+        else:
+            print(i, 'no es primo')
+numeros_primos(int(input('ingresa el valor: ')))
