7a5eae1a6e5842048699f4e4748229b5e0abe8b3
heberjfet <heber.alvaro49@unach.mx>
2022-10-23 02:17:32 -0500

    avanzar

--- script-216.py
+++ script-216.py
@@ -3,9 +3,19 @@
 #decripcion: matrices
 #fecha: 19/10
 
-cadena1 = 'hola'
-cadena2 = 'HEBER jafet'
-#print(cadena1.capitalize())
-#print(cadena2.capitalize())
-#print(cadena2.lower())
-print(cadena1.upper())
+cadena1 = 'hola'.upper()
+cadena2 = "heber jafet es una prueba de front".capitalize()
+cadena3 = 'edad:'.upper()
+# print(cadena1.capitalize())
+# print(cadena2.capitalize())
+# print(cadena2.lower())
+# print(cadena1.upper())
+# print(cadena2.swapcase())
+print(cadena1.center(50,"-"))
+# print(cadena2.center(50,"°"))
+print(cadena2.ljust(30,"*"))
+# print(cadena3.rjust(30,'-'))
+# # print(str(cadena3).zfill(25))
+# pru = str(input('nombre de usuario: '))
+# print(pru.count('e'))
+print(cadena2.find('una',0,20))
