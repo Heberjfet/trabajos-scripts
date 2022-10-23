fe5d46ab752fb04e5e73c16e6343b8da396e64d2
Heber Jafet Jafet <114103375+Heberjfet@users.noreply.github.com>
2022-10-23 04:08:52 +0000

    Ok

---
+++ script-216.py
@@ -0,0 +1,11 @@
+#programa: script-215
+#programador: Heber Jafet Alvaro Ramirez
+#decripcion: matrices
+#fecha: 19/10
+
+cadena1 = 'hola'
+cadena2 = 'HEBER jafet'
+#print(cadena1.capitalize())
+#print(cadena2.capitalize())
+#print(cadena2.lower())
+print(cadena1.upper())
