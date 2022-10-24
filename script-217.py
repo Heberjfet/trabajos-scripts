1f59ac281e14d8798685e55c4f3bb5133972d3f8
heberjfet <heber.alvaro49@unach.mx>
2022-10-24 01:06:35 -0500

    revisar

--- script-217.py
+++ script-217.py
@@ -2,6 +2,7 @@
 #programador: Heber Jafet Alvaro Ramirez
 #decripcion: matrices
 #fecha: 24/10/22
+
 h = 'usuario'.upper()
 print(h.center(50,"-"))
 
@@ -15,4 +16,13 @@ def usuario(dato):
     if conte > 12:
         print('el usuario solo puede contener max 12 carecteres')
 
-usuario(input('nombre de usuario: '.title()))
+# usuario(input('nombre de usuario: '.title()))
+
+def contraseña(dato1):
+    # mayusculas= (count(dato1.isupper))
+    minisculas = dato1.islower()
+    espacios = dato1.isspace()
+    conteo = len(dato1.upper)
+    print(conteo)
+
+contraseña(input('ingresa la contraseña: '))
