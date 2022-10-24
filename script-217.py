f9cd673734b470008e806605972ed9357f4b2c39
heberjfet <heber.alvaro49@unach.mx>
2022-10-24 00:45:49 -0500

    mejorar el script 217

---
+++ script-217.py
@@ -0,0 +1,18 @@
+#programa: script-215
+#programador: Heber Jafet Alvaro Ramirez
+#decripcion: matrices
+#fecha: 24/10/22
+h = 'usuario'.upper()
+print(h.center(50,"-"))
+
+def usuario(dato):
+    validacion2 = dato.isalnum()
+    conte=len(dato)
+    if validacion2 != True:
+        print('solo se permiten letras y numeros')
+    if conte < 6:
+        print('el usuario minimo es de 6 caracteres')
+    if conte > 12:
+        print('el usuario solo puede contener max 12 carecteres')
+
+usuario(input('nombre de usuario: '.title()))
