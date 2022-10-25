ce477873d174105c35e215d96da29c3d81f43ba6
heberjfet <heber.alvaro49@unach.mx>
2022-10-25 16:35:24 -0500

    revisado

--- script-204.py
+++ script-204.py
@@ -1,23 +1,31 @@
-#programa: script-128
-#programador: Heber Jafet Alvaro Ramirez
-#decripcion:
-#fecha: 05/10/2022
+#programa: script-204
+#programador: heber jafet alvaro ramirez
+#descripcion: Dados 2 números diga si son amigos, recuerda que dos números son amigos si la suma de sus divisores de uno de ello es igual al otro y viceversa
+#fecha de entrega: 05/10/22
 
-n1 = int (input('dijite un valor: '))
-n2 = int (input('dijite un valor: '))
-r = []
-i = 1
-for r in i <= n1 / 2:
-    n1 % i == 0
-    s1 = s1 + 1
-    i = i + 1
-    i = 1
-    i <= n2 / 2
-    n2 % i == 0
-    s2 = s2 + i
-    i = i + 1
-    n1 == s2 and n2==s1
-    r = ('no son amigos')
-    r = ('son amigos')
-print (r)
 
+n1=int(input('numero1: '))
+n2=int(input('numero2: '))
+
+
+list_divisores=[]
+list_divisores2=[]
+
+for t in range (1,n1):
+    if n1 % t ==0:
+        list_divisores.append(t)
+        suma1=sum(list_divisores)
+for i in range (1,n2):
+    if n2 % i ==0:
+        list_divisores2.append(i)
+        suma2=sum(list_divisores2)
+
+if suma1==n2 and suma2==n1:
+    print('Son amigos')
+else:
+    print('No son amigos') 
+#list-divisores.append(t), es para sumar los elemntos de la lista
+#Para append se usa parentesis
+#Mod (%) son para el residuo de las divisiones
+#sum, para sumar
+#Las variables t, i, seran almacenedores de los rangos
