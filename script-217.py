a3dc5388479a7484a5c8c5e7374fe25bad795f5e
heberjfet <heber.alvaro49@unach.mx>
2022-10-25 00:30:14 -0500

    revisar

--- script-217.py
+++ script-217.py
@@ -8,27 +8,33 @@ print(h.center(50, "-"))
 
 
 def usuario(dato):
-  validacion2 = dato.isalnum()
-  conte = len(dato)
-  if validacion2 != True:
-    print('solo se permiten letras y numeros')
-  if conte < 6:
-    print('el usuario minimo es de 6 caracteres')
-  if conte > 12:
-    print('el usuario solo puede contener max 12 carecteres')
+    validacion2 = dato.isalnum()
+    conte = len(dato)
+    if validacion2 != True:
+        print('solo se permiten letras y numeros')
+    if conte < 6:
+        print('el usuario minimo es de 6 caracteres')
+    if conte > 12:
+        print('el usuario solo puede contener max 12 carecteres')
 
 
 # usuario(input('nombre de usuario: '.title()))
 
 
 def contraseña(dato1):
-  conpro = dato1.isalnum()
-  mayusculas = dato1.isupper()
-  print(mayusculas)
-  #minisculas = dato1.islower()
-  #espacios = dato1.isspace()
-  #conteo = len(dato1.upper)
-  print(conpro)
+    conta = len(dato1)
+    conpro = dato1.isalpha()
+    mayusculas = dato1.islower()
+    num = dato1.isdigit()
+    if conta < 8:
+        print('anañade mas caracteres')
+    elif conpro == True :
+        print('añade un numero')
+    elif mayusculas != False:
+        print('añade una letra mayuscula')
+    elif num == True:
+        print('añade letras y numeros')
+    
 
 
 contraseña(input('ingresa la contraseña: '))
