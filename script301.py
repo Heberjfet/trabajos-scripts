02d80c31d273f1ccbcc5b83d2cad7a8d6e963238
Heber Jafet Jafet <114103375+Heberjfet@users.noreply.github.com>
2022-11-07 17:00:57 +0000

    debug

--- script301.py
+++ script301.py
@@ -3,14 +3,19 @@
 #descripcion: Funciones Recursivas
 #Fecha: Lunes 7 de Noviembre de 2022
 
-def factorial (numero):
-    if numero >1:
-        return(numero*factorial(numero-1))
-    else:
-        return (numero)
+
+def factorial(numero):
+  if numero > 1:
+    return (numero * factorial(numero - 1))
+  else:
+    return (numero)
+
 
 numero = int(input('introduzca un numero al que le calculara su factorial: '))
-if numero < 0 :
-    print('No existe el factorial para su numero negativos') #Condicion para los numeros negativos
+if numero < 0:
+  print('No existe el factorial para su numero negativos'
+        )  #Condicion para los numeros negativos
 elif numero == 0:
-    print('El factorial de', numero, 'es 1')
+  print('El factorial de', numero, 'es 1')
+else:
+  print('')
