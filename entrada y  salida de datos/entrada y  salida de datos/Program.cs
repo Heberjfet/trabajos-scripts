b5266e002be742fdb7e0799e24f0cd217ffda1e0
heberjfet <heber.alvaro49@unach.mx>
2023-02-13 08:15:29 -0600

    revisar

---
+++ entrada y  salida de datos/entrada y  salida de datos/Program.cs
@@ -0,0 +1,19 @@
+﻿// heber jafet alvaro ramirez 
+namespace entrada_y_salida_de_datos
+{
+
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, apellido;
+            Console.WriteLine("hola mundo");
+            Console.WriteLine("Ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingresa tu apellido: ");
+            apellido = Console.ReadLine();
+
+            Console.WriteLine("tu nombre es: " + nombre + "tu apellido es:" + apellido);
+        }
+    }
+}
